4f7dab7d762a3c88ab0b12965082689cb5951437
Ben Choi <choiben314@gmail.com>
2016-07-23 19:27:36 -0400

    Loop basic sound.

--- app/src/main/java/com/example/ben/metronome/MainActivity.java
+++ app/src/main/java/com/example/ben/metronome/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.ben.metronome;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +21,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        final PlaySound play = new PlaySound();
+        play.mPlayer = MediaPlayer.create(this, R.raw.woodblock);
+        play.play_button = (Button)this.findViewById(R.id.button);
+
+        play.play_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                play.mPlayer.start();
             }
         });
+        play.mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        new Timer().scheduleAtFixedRate(play, 0, 1000);
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
