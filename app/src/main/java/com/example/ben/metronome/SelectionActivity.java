e53cc0ac8d35d83b05dde8e59048770402936bbe
Ben Choi <Ben Choi>
2016-09-17 21:58:54 -0400

    Create demo version.

--- app/src/main/java/com/example/ben/metronome/SelectionActivity.java
+++ app/src/main/java/com/example/ben/metronome/SelectionActivity.java
@@ -35,7 +35,7 @@ public class SelectionActivity extends AppCompatActivity {
                     Intent intent = new Intent(SelectionActivity.this, EditActivity.class);
                     Piece piece = new Piece ("My Piece", new Default(4, 4, 100), new ArrayList<Exception>(), 3);
                     listItems.add(piece);
-                    intent.putExtra("piece", piece);
+                    intent.putExtra("Piece", piece);
                     startActivity(intent);
                 }
             });
@@ -58,8 +58,27 @@ public class SelectionActivity extends AppCompatActivity {
         arr.add(ex3);
         arr.add(ex4);
 
+        ArrayList<Exception>arr1 = new ArrayList<Exception>();
+        Exception exc1 = new Exception (7, 8, 180, 4, 8);
+        Exception exc2 = new Exception (5, 8, 120, 10, 12);
+
+        arr1.add(exc1);
+        arr1.add(exc2);
+
         Piece piece = new Piece ("Stravinsky 3 Pieces for Clarinet", new Default(2, 4, 52), arr, 1000);
+        Piece piece1 = new Piece ("Mozart", new Default(3, 4, 180), arr, 1000);
+        Piece piece2 = new Piece ("Beethoven Symphony No.8", new Default(5, 4, 90), arr, 1000);
+        Piece piece3 = new Piece ("Bartok Concerto", new Default(6, 4, 120), arr, 1000);
+        Piece piece4 = new Piece ("String Quartet", new Default(3, 2, 67), arr, 1000);
+        Piece piece5 = new Piece ("Debussy Clair de Lune", new Default(2, 4, 52), arr, 1000);
+        Piece piece9 = new Piece ("My New Piece 1", new Default(4, 4, 100), arr1, 1000);
         listItems.add(piece);
+        listItems.add(piece1);
+        listItems.add(piece2);
+        listItems.add(piece3);
+        listItems.add(piece4);
+        listItems.add(piece5);
+        listItems.add(piece9);
 
         listAdapter = new ArrayAdapter<Piece>(this, R.layout.simplerow, listItems);
 
